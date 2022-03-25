71763561b8f113d92e7cef1330fc3a38d3efdfa2
Mostakim <mostak03@gmail.com>
2022-03-25 14:41:53 +0600

    metrics path has been made pointer string

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -10,14 +10,14 @@ const (
 )
 
 // NewEchoPrometheusClient adds metricsPath to echo server as middleware
-func NewEchoPrometheusClient(e *echo.Echo, metricsPath string) {
+func NewEchoPrometheusClient(e *echo.Echo, metricsPath *string) {
 	prom := prometheus.NewPrometheus(promSubsystemName, nil)
 
 	// Scrape metrics from Main Server
 	e.Use(prom.HandlerFunc)
 
-	if metricsPath != "" {
-		prom.MetricsPath = metricsPath
+	if metricsPath != nil {
+		prom.MetricsPath = *metricsPath
 	}
 	// Setup metrics endpoint at application server
 	prom.SetMetricsPath(e)
